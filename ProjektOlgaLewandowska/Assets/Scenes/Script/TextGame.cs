ac3a5a01fa39085eb3581e65ce606404fdc3f3af
Olga <olgalewandowska17l@interia.pl>
2022-01-18 17:06:10 +0100

    Zajęcia 4

--- ProjektOlgaLewandowska/Assets/Scenes/Script/TextGame.cs
+++ ProjektOlgaLewandowska/Assets/Scenes/Script/TextGame.cs
@@ -7,19 +7,70 @@ public class TextGame : MonoBehaviour
     //Minimalna i maksymalna wartoœæ
     int min = 1;
     int max = 1000;
-    int guess = 500; 
+    int guess = 500;
+    string choice;
+
+    void GuessScope()
+    {
+        Debug.Log("Podaj z jakiego zakresu ma zgadywaæ komputer");
+        Debug.Log("Jeœli od 1 do 50 to naciœnij klawisz 1");
+        Debug.Log("Jeœli od 1 do 1000 to naciœnij klawisz 2");
+        Debug.Log("Jeœli od 1 do 10000 to naciœnij klawisz 3");
+        if (choice == "1")
+        {
+
+        }
+        if (true)
+        {
+
+        }
+        if (true)
+        {
+
+        }
+
+        switch (choice)
+            {
+            case "1":
+                min = 1;
+                max = 50;
+                break;
+            case "2":
+                min = 1;
+                max = 1000;
+                break;
+            case "3":
+                min = 1;
+                max = 10000;
+                break;
+
+        }
+
+    }
+
+    void NextGuess()
+    {
+        guess = (min + max) / 2;
+        Debug.Log("Czy twoja liczba jest wieksza od " + guess + "?");
+    }
+
+    void StartGame()
+    {
+        guess = (min + max) / 2;
+        Debug.Log("Witaj w mojej grze");
+        Debug.Log("Wymyœl  w g³owie liczbê od  " + min + " do  " + max);
+        Debug.Log("Czy twoja liczba jest wiêksza od " + guess + "?");
+        Debug.Log("Nacisnij górn¹ strza³kê jeœli twoja liczba jest wiêksza \n");
+        Debug.Log("Naciœnij doln¹ strza³kê jeœli jest mniejsza \n");
+        Debug.Log("Naciœnij enter jeœli jest równa czyli program zgadnie twoj¹ liczbê ");
+        max++;//zwieksz wartoœæ o 1
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-        guess = (min + max) / 2;
-        Debug.Log("Witaj w mojej grze");
-        Debug.Log("Wymyœl  w g³owie liczbê od  "+min+" do  "+max);
-        Debug.Log("Czy twoja liczba jest wiêksza od " +guess);
-        Debug.Log("Nacisnij górn¹ stra³kê jeœli twoja liczba jest wiêksza \n" +
-                 "Naciœnij doln¹ strza³kê jeœli jest mniejsza \n");
-        Debug.Log("Naciœnij enter jeœli jest równa czyli program zgadnie twoj¹ liczbê ");
 
+        StartGame();
         
     }
 
@@ -30,8 +81,7 @@ public class TextGame : MonoBehaviour
         {
            
             min = guess;
-            guess = (min + max) / 2;
-            Debug.Log("Czy twoja liczba jest wieksza od " +guess );
+            NextGuess();
            
 
         }
@@ -39,9 +89,8 @@ public class TextGame : MonoBehaviour
         {
             
             max = guess;
-            guess = (min + max) / 2;
-            Debug.Log("Czy twoja liczba jest wieksza od " +guess);
-           
+            NextGuess();
+
         }
         if (Input.GetKeyDown(KeyCode.Return))
         {
